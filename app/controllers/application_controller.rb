1c86a74a38b1cc8813a93d97303adffa6b55b701
Sh2kari <buba_sev@bk.ru>
2015-10-24 11:39:04 +0300

    cart controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,16 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+  helper_method :categories
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :name, :email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
+  end
+
+  def categories
+    @categories = Category.all
   end
 
   def weekend
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,7 @@
 class Cart < ActiveRecord::Base
   has_many :line_items
+
+  def total_price
+    line_items.to_a.sum{ |item| item }
+  end
 end
